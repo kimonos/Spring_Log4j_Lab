7ae60defdd095f297f7d6cc6d86158a587ada68d
qiuyouxuan <youhappy421@gmail.com>
2024-10-31 18:09:28 +0900

    test

--- src/main/java/com/gss/demo/controller/sqlController.java
+++ src/main/java/com/gss/demo/controller/sqlController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class sqlController {
 
@@ -19,8 +21,12 @@ public class sqlController {
     public User findUserFromId(@PathVariable("id") Long id){
 
 //        return userRepository.findById(id).orElse(null);
-        return userRepository.findById(id)
-                .map(ResponseEntity::ok) // 返回用戶對象
-                .orElse(ResponseEntity.notFound().build()).getBody();
+        // 使用 Optional 進行查詢
+        Optional<User> userOptional = userRepository.findById(id);
+
+        // 處理查詢結果
+        return userOptional
+                .map(ResponseEntity::ok) // 如果存在用戶，返回200及用戶信息
+                .orElse((ResponseEntity<User>) ResponseEntity.status(404)).getBody(); // 如果不存在，返回404狀態
     }
 }
