0df2548743a30368240fde17b01f6c583a181c95
qiuyouxuan <youhappy421@gmail.com>
2024-10-31 16:19:53 +0900

    test

--- src/main/java/com/gss/demo/controller/demoController.java
+++ src/main/java/com/gss/demo/controller/demoController.java
@@ -8,6 +8,6 @@ public class demoController {
 
     @GetMapping("/view")
     public String view() {
-        return "hello world test ryuta test";
+        return "hello world test ryuta";
     }
 }
