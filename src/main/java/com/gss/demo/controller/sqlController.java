340bf4ccdf743254dbbc3011f51b814e57b4770a
qiuyouxuan <youhappy421@gmail.com>
2024-10-31 17:59:27 +0900

    test

--- src/main/java/com/gss/demo/controller/sqlController.java
+++ src/main/java/com/gss/demo/controller/sqlController.java
@@ -4,6 +4,7 @@ package com.gss.demo.controller;
 import com.gss.demo.enity.User;
 import com.gss.demo.repository.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,10 @@ public class sqlController {
 
     @GetMapping("/user/{id}")
     public User findUserFromId(@PathVariable("id") Long id){
-        return userRepository.findById(id).orElse(null);
+
+//        return userRepository.findById(id).orElse(null);
+        return userRepository.findById(id)
+                .map(ResponseEntity::ok) // 返回用戶對象
+                .orElse(ResponseEntity.notFound().build()).getBody();
     }
 }
