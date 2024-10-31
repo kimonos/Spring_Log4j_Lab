1cfb2eecfd2264b744c2f1e8ce01d35cd7c5ef26
qiuyouxuan <youhappy421@gmail.com>
2024-10-31 18:15:14 +0900

    test

--- src/main/java/com/gss/demo/controller/sqlController.java
+++ src/main/java/com/gss/demo/controller/sqlController.java
@@ -5,28 +5,50 @@ import com.gss.demo.enity.User;
 import com.gss.demo.repository.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+//@RestController
+//public class sqlController {
+//
+//    @Autowired
+//    private userRepository userRepository;
+//
+//    @GetMapping("/user/{id}")
+//    public User findUserFromId(@PathVariable("id") Long id){
+//
+////        return userRepository.findById(id).orElse(null);
+//        // 使用 Optional 進行查詢
+//        Optional<User> userOptional = userRepository.findById(id);
+//
+//        // 處理查詢結果
+//        return userOptional
+//                .map(user -> userRepository.ok(user)) // 如果存在用戶，返回200及用戶信息
+//                .orElse(ResponseEntity.status(404).body("User not found.")); // 如果不存在，返回404狀態
+//    }
+//}
 @RestController
+@Validated
 public class sqlController {
 
     @Autowired
     private userRepository userRepository;
 
     @GetMapping("/user/{id}")
-    public User findUserFromId(@PathVariable("id") Long id){
+    public ResponseEntity<User> findUserFromId(
+            @PathVariable("id")
+            Long id) {
 
-//        return userRepository.findById(id).orElse(null);
-        // 使用 Optional 進行查詢
-        Optional<User> userOptional = userRepository.findById(id);
+        if (id == null) {
+            return ResponseEntity.badRequest().build();
+        }
 
-        // 處理查詢結果
-        return userOptional
-                .map(ResponseEntity::ok) // 如果存在用戶，返回200及用戶信息
-                .orElse((ResponseEntity<User>) ResponseEntity.status(404)).getBody(); // 如果不存在，返回404狀態
+        return userRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
