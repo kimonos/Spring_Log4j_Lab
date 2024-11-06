6661c7cff4fed8cb1a80f7de8724f70a7cee1529
qiuyouxuan <youhappy421@gmail.com>
2024-11-06 16:26:57 +0900

    test

--- src/main/java/com/gss/demo/repository/UserRepository.java
+++ src/main/java/com/gss/demo/repository/UserRepository.java
@@ -2,7 +2,13 @@ package com.gss.demo.repository;
 
 import com.gss.demo.enity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findUserById(@Param("id") Long id);
 }
